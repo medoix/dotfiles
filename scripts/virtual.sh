e4151a426b7bd2494f5d19f5ec43694e31e6a0cf
medoix <medoix@shivv.com>
2021-04-08 22:38:38 +1000

    Added virtualbox guest setup support

---
+++ scripts/virtual.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Configure setting to run inside a VirtualBox Machine
+VBoxClient --clipboard
+VBoxClient --draganddrop
+VBoxClient --seamless
+VBoxClient --checkhostversion
+VBoxClient --vmsvga
