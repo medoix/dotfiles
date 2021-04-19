203d1b2194fc85a4e22e0ba71132701a886b2d3f
medoix <medoix@shivv.com>
2021-04-19 21:22:10 +1000

    Configure Virtual1 if running in VirtualBox and decrease DPI to 115

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -2,7 +2,9 @@
 intern=eDP-1
 extern=HDMI-1
 
-if xrandr | grep "$extern disconnected"; then
+if xrandr | grep "Virtual1 connected"; then
+    xrandr --output "Virtual1" --output 1920x1200 --dpi 115
+elif xrandr | grep "$extern disconnected"; then
     xrandr --output "$extern" --off --output "$intern" --auto
 else
     xrandr --output "$intern" --off --output "$extern" --auto
