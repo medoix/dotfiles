856944f2d56c313070ea85748243228a4451de77
medoix <medoix@shivv.com>
2021-04-20 08:01:04 +1000

    Correct script paramter to use --mode for resolution

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -3,7 +3,7 @@ intern=eDP-1
 extern=HDMI-1
 
 if xrandr | grep "Virtual1 connected"; then
-    xrandr --output "Virtual1" --output 1920x1200 --dpi 115
+    xrandr --output "Virtual1" --mode 1920x1200 --dpi 115
 elif xrandr | grep "$extern disconnected"; then
     xrandr --output "$extern" --off --output "$intern" --auto
 else
