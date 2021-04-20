9eac44e369ea5500d35c25b83c69827ca9c70908
medoix <medoix@shivv.com>
2021-04-20 07:59:19 +0000

    Dual monitor at desk setup

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+virtual=Virtual1
 intern=eDP-1
-extern=HDMI-1
+extern1=DP-1
+extern2=DP-2
 
-if xrandr | grep "Virtual1 connected"; then
-    xrandr --output "Virtual1" --mode 1920x1200 --dpi 115
-elif xrandr | grep "$extern disconnected"; then
-    xrandr --output "$extern" --off --output "$intern" --auto
+if xrandr | grep "$virtual connected"; then
+    xrandr --output "$virtual" --mode 1920x1200 --dpi 115
+elif xrandr | grep "$extern1 connected"; then
+    xrandr --output "$intern" --mode 1920x1080 --dpi 115
+    xrandr --output "$extern1" --scale 0.8 --above "$intern" --auto
+elif xrandr | grep "$extern2 connected"; then
+    xrandr --output "$intern" --mode 1920x1080 --dpi 115 
+    xrandr --output "$extern2" --above "$intern" --auto
 else
-    xrandr --output "$intern" --off --output "$extern" --auto
+    xrandr --output "$intern" --mode 1920x1080 --dpi 115
 fi
