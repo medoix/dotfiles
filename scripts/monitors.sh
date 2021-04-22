2231a5751080243969b0c0e901258a0587525dc2
medoix <medoix@shivv.com>
2021-04-22 07:34:45 +0000

    Adjust dual monitor setup scaling/positioning

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -8,7 +8,8 @@ if xrandr | grep "$virtual connected"; then
     xrandr --output "$virtual" --mode 1920x1200 --dpi 115
 elif xrandr | grep "$extern1 connected"; then
     xrandr --output "$intern" --mode 1920x1080 --dpi 115
-    xrandr --output "$extern1" --scale 0.7 --above "$intern" --auto
+    #xrandr --output "$extern1" --scale 0.7 --above "$intern" --auto
+    xrandr --output "$extern1" --scale 1 --above "$intern" --mode 2560x1440
 elif xrandr | grep "$extern2 connected"; then
     xrandr --output "$intern" --mode 1920x1080 --dpi 115 
     xrandr --output "$extern2" --above "$intern" --auto
