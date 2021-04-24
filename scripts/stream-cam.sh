acf2a9c50046d0b83fb18c98f2f03e813c8b0a7c
medoix <medoix@shivv.com>
2021-04-24 21:45:17 +0000

    Add external USB camera

--- scripts/stream-cam.sh
+++ scripts/stream-cam.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-mpv /dev/video0 --hue=-9 --video-zoom=0.2 &
+
+# Laptop Camera
+#mpv /dev/video0 --hue=-9 --video-zoom=0.2 &
+
+# External USB Camera
+mpv /dev/video4 --hue=-9 --video-zoom=0.2 &
